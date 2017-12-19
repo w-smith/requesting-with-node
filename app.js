a8116f2d71f1a3a59ba363c31ae64bd1df8e132c
w-smith <wsmith@bex.net>
2017-12-19 16:43:14 -0700

    adding code and key

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// PROJECT CODE
+// 016144505224963957432:ndkflwf3wi0
+
+// KEY
+// AIzaSyABvwQIGurrWIMwkO8ONlhKgv2kf6Xt6bg
+
 var movie = require("./movie");
 var threeFavoriteMovies = [];
 threeFavoriteMovies.forEach(function(film){
