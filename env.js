3aee70bc3d5d6bbba2fbdce9328e20a807337d4c
w-smith <wsmith@bex.net>
2017-12-19 17:45:28 -0700

    no idea what im doing.

---
+++ env.js
@@ -0,0 +1,11 @@
+// f your `env.js` is still on GitHub, but you put it in `.gitignore`:
+
+// https://stackoverflow.com/questions/1274057/how-to-make-git-forget-about-a-file-that-was-tracked-but-is-now-in-gitignore
+
+const api = "016144505224963957432:ndkflwf3wi0";
+
+module.exports = api;
+
+const key = "AIzaSyABvwQIGurrWIMwkO8ONlhKgv2kf6Xt6bg";
+
+module.exports = key;
---
+++ movie.js
@@ -0,0 +1,22 @@
+const api = require("./env");
+console.log(api);
+
+
+const key = require("./env");
+console.log(key);
+
+// module.exports(); {
+
+// 	get(movieTitle); {
+
+// request("http://api.wunderground.com/api/25b9b9cdc6cb394b/conditions/q/CO/Denver.json", function (error, response, body)
+
+
+// 	}
+// }
+
+
+
+
+
+var request = require("request");
