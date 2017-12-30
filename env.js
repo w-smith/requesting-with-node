89a7cae50600721c68eea03609bbc1ff48e20042
w-smith <wsmith@bex.net>
2017-12-30 15:19:33 -0700

    adding files to gitignore

--- env.js
+++ env.js
@@ -1,7 +1,3 @@
-// f your `env.js` is still on GitHub, but you put it in `.gitignore`:
-
-// https://stackoverflow.com/questions/1274057/how-to-make-git-forget-about-a-file-that-was-tracked-but-is-now-in-gitignore
-
 module.exports = {
 
 api : "016144505224963957432:ndkflwf3wi0",
